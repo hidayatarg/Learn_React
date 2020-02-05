be5a38164bbfd3c5035ef2ba43f4bab3b90d06e7
Hidayat Arghandabi <hidayatarg@gmail.com>
2020-02-05 17:33:25 +0300

    Webpack Configuration for Development

---
+++ webpack.config.dev.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+process.env.NODE_ENV = 'development'
+
+module.exports = {
+    mode: 'development',
+    target: 'web',
+    // recommended for development
+    devTool: 'cheap-module-source-map',
+    entry: './src/index.js',
+    output: {
+        // serving from memory
+        path: path.resolve(__dirname, "build"),
+        // public url path
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        stats: 'minimal',
+        overlay: true,
+        historyApiFallback: true,
+        // due to web pack issue
+        disableHostCheck: true,
+        headers: { "Access-Control-Allow-Origin": "*" },
+        https: false
+    }, 
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "src/index.html",
+            favicon: "src/favicon.ico"
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader", "eslint-loader"]
+            },
+            {
+                test: /(\.css)$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    }
+}
