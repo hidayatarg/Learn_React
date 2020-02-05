92f1b125fa5e73877500d954d26e35492300320f
Hidayat Arghandabi <hidayatarg@gmail.com>
2020-02-05 21:48:12 +0300

    EsLint Configuration

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -37,7 +37,7 @@ module.exports = {
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: ["babel-loader"]
+                use: ["babel-loader", "eslint-loader"]
             },
             {
                 test: /(\.css)$/,
