4d808dc23d4a2dd12c9d70009c5e2b4360603485
Hidayat Arghandabi <hidayatarg@gmail.com>
2020-02-06 15:12:26 +0300

    Create Component and Link to Nav - Courses Component

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import HomePage from './home/HomePage'
 import AboutPage from './about/AboutPage'
 import Header from './common/Header'
 import PageNotFound from './PageNotFound'
+import CoursesPage from './courses/CoursesPage'
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
             <Switch>
                 <Route exact path="/" component={HomePage} /> 
                 <Route path="/about" component={AboutPage} /> 
+                <Route path="/courses" component={CoursesPage} /> 
                 <Route component={PageNotFound} /> 
             </Switch>
         </div>
---
+++ src/components/courses/CoursesPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class CoursesPage extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Course Page</h2>
+            </div>
+        )
+    }
+}
+
+export default CoursesPage
