205a3e3b8311a9e20a9809cae89d11f342af5e14
Hidayat Arghandabi <hidayatarg@gmail.com>
2020-02-08 00:30:01 +0300

    Create Simple Add Course Form

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react'
 
 class CoursesPage extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            course: {
+                title: ''
+            }
+        }
+    }
+    
+    handleChange(event){
+        // create copy of existing state
+        // update the title with event 
+        // you edit any object in the data new state
+        const course = {...this.state.course, title: event.target.value}
+        this.setState({
+            course: course
+        })
+    }
+
     render() {
         return (
-            <div>
-                <h2>Course Page</h2>
-            </div>
+            <form>
+                <h2>Courses</h2>
+                <h3>Add Course</h3>
+                <input 
+                    type="text" 
+                    onChange={this.handleChange} 
+                    value={this.state.course.title} 
+                />
+                <input 
+                    type="submit" 
+                    value="Save" 
+                />
+            </form>
         )
     }
 }
