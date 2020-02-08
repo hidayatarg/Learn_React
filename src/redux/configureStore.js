adfa19cd6c74dd8332f8ceeee8b2e334dfba4a7a
Hidayat Arghandabi <hidayatarg@gmail.com>
2020-02-08 18:02:27 +0300

    fixed the [redux-immutable-state-invariant] Typo

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './reducers'
-import reduxImmutableStateInvariant from 'redux-immutable-state-invarent'
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant'
 
 export default function configureStore(initalState) {
     // add support for redux dev tools
